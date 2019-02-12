f04bf2ec19224550554d78ba3c5f22a3e1c3b9fd
naffiq <abdu.galymzhan@gmail.com>
2019-02-12 22:30:41 +0100

    feat(scene): limit zoom in/out and resize handler

--- src/main.js
+++ src/main.js
@@ -1,80 +1,89 @@
-import * as THREE from 'three'
-import * as WindowResize from 'three-window-resize'
-import OrbitControlsSetup from 'three-orbit-controls'
-const OrbitControls = OrbitControlsSetup(THREE)
+import * as THREE from "three";
+import OrbitControlsSetup from "three-orbit-controls";
+const OrbitControls = OrbitControlsSetup(THREE);
+OrbitControls.minDistance = 10;
 
-import earthMaterial from './materials/earthMaterial'
-import cloudMaterial from './materials/cloudMaterial'
+import earthMaterial from "./materials/earthMaterial";
+import cloudMaterial from "./materials/cloudMaterial";
 
-var scene, renderer
-var cameraAspect, camera, cameraControl
+var scene, renderer;
+var cameraAspect, camera, cameraControl;
 
-const sphereWidthSegments = 30
-const sphereHeightSegments = 30
-var sphereGeometry = new THREE.SphereGeometry(15, 30, 30)
-var earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial)
-earthMesh.name = 'earth'
+const sphereWidthSegments = 30;
+const sphereHeightSegments = 30;
+var sphereGeometry = new THREE.SphereGeometry(15, 30, 30);
+var earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+earthMesh.name = "earth";
 
-const cloudGeometryRadius = sphereGeometry.parameters.radius * 1.01
+const cloudGeometryRadius = sphereGeometry.parameters.radius * 1.01;
 var cloudGeometry = new THREE.SphereGeometry(
-  cloudGeometryRadius, sphereWidthSegments, sphereHeightSegments
-)
-var cloudMesh = new THREE.Mesh(cloudGeometry, cloudMaterial)
-cloudMesh.name = 'clouds'
+  cloudGeometryRadius,
+  sphereWidthSegments,
+  sphereHeightSegments
+);
+var cloudMesh = new THREE.Mesh(cloudGeometry, cloudMaterial);
+cloudMesh.name = "clouds";
 
-var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1)
-directionalLight.name = 'directional'
-directionalLight.position.copy(new THREE.Vector3(100, 10, -50))
+var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.name = "directional";
+directionalLight.position.copy(new THREE.Vector3(100, 10, -50));
 
 function init() {
-  scene = new THREE.Scene()
+  scene = new THREE.Scene();
 
-  renderer = new THREE.WebGLRenderer
-  renderer.setClearColor(0x000000)
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor(0x000000);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  cameraAspect = window.innerWidth / window.innerHeight;
 
-  cameraAspect = window.innerWidth / window.innerHeight
-  camera = new THREE.PerspectiveCamera(45, cameraAspect)
-  camera.position.x = 30
-  camera.position.y = 0
-  camera.position.z = 15
+  camera = new THREE.PerspectiveCamera(45, cameraAspect);
+  camera.position.x = 30;
+  camera.position.y = 0;
+  camera.position.z = 15;
+  camera.lookAt(scene.position);
 
-  camera.lookAt(scene.position)
+  cameraControl = new OrbitControls(camera);
+  cameraControl.minDistance = 30;
+  cameraControl.maxDistance = 100;
 
-  cameraControl = new OrbitControls(camera)
+  scene.add(earthMesh);
+  scene.add(cloudMesh);
 
-  scene.add(earthMesh)
-  scene.add(cloudMesh)
+  scene.add(directionalLight);
 
-  scene.add(directionalLight)
+  var ambientLight = new THREE.AmbientLight(0x111111);
+  scene.add(ambientLight);
 
-  var ambientLight = new THREE.AmbientLight(0x111111)
-  scene.add(ambientLight)
-
-  document.body.appendChild(renderer.domElement)
-  render()
+  document.body.appendChild(renderer.domElement);
+  render();
 }
 
 function render() {
-  requestAnimationFrame(render)
-  cameraControl.update()
+  requestAnimationFrame(render);
+  cameraControl.update();
 
-  earthMesh.rotation.y += .0005;
-  cloudMesh.rotation.y += .00052;
+  earthMesh.rotation.y += 0.0005;
+  cloudMesh.rotation.y += 0.00052;
 
-  localStorage.setItem('earthRotation', earthMesh.rotation.y);
-  localStorage.setItem('cloudsRotation', cloudMesh.rotation.y);
-
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
 }
 
-document.addEventListener('resize', function () {
-  renderer.setSize(window.innerWidth, window.innerHeight)
+document.addEventListener("resize", function() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-  cameraAspect = window.innerWidth / window.innerHeight
-  camera.aspect = cameraAspect
-  camera.updateProjectionMatrix()
-})
+  cameraAspect = window.innerWidth / window.innerHeight;
+  camera.aspect = cameraAspect;
+  camera.updateProjectionMatrix();
+});
 
-init()
-var windowResize = new WindowResize(renderer, camera)
+init();
+
+window.addEventListener(
+  "resize",
+  () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  },
+  false
+);
